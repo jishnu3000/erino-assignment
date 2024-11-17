d70c904364ef012ef1a121b8a23470fec0db37c4
Jishnu <jishnujayaraj455@gmail.com>
2024-11-17 13:45:21 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:3003",
+        target: "http://localhost:3003",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
