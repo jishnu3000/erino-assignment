796cc0560e050035224c36d10302c80264c703a2
Jishnu <jishnujayaraj455@gmail.com>
2024-11-17 13:32:58 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3003",
+        target: process.env.VITE_API_URL || "http://localhost:3003",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
