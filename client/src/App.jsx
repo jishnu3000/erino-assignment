bfc2e1f2c7cd6207c9d660f7e038cc62d2177f2d
Jishnu <jishnujayaraj455@gmail.com>
2024-11-17 11:57:07 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ContactForm from "./components/ContactForm";
 import ContactsTable from "./components/ContactsTable";
 import { Container, Typography, Box } from "@mui/material";
+import contactService from './services/contacts';
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const data = await contactService.getAll();
+        setContacts(data);
+      } catch (error) {
+        console.error("Error when fetching contacts: ", error);
+      }
+    }
+    fetchContacts();
+  }, [])
   
-  const handleSaveContact = (newContact) => {
-    setContacts((prev) => [...prev, newContact]);
+  const handleSaveContact = async (newContact) => {
+    try {
+      const savedContact = await contactService.create(newContact);
+      setContacts((prev) => [...prev, savedContact]);
+    } catch (error) {
+      console.error("Error saving contact:", error);
+    }
   };
 
-  const handleUpdateContact = (updatedContact) => {
-    setContacts((prev) =>
-      prev.map((contact) =>
-        contact.email === updatedContact.email ? updatedContact : contact
-      )
-    );
+  const handleUpdateContact = async (updatedContact) => {
+    try {
+      const { id } = updatedContact;
+      const savedContact = await contactService.update(id, updatedContact);
+      setContacts((prev) =>
+        prev.map((contact) => (contact.id === id ? savedContact : contact))
+      );
+    } catch (error) {
+      console.error("Error updating contact:", error);
+    }
   };
 
-  const handleDeleteContact = (contactToDelete) => {
-    setContacts((prev) =>
-      prev.filter((contact) => contact.email !== contactToDelete.email)
-    );
+  const handleDeleteContact = async (contactToDelete) => {
+    try {
+      const { id } = contactToDelete;
+      await contactService.deleteContact(id);
+      setContacts((prev) => prev.filter((contact) => contact.id !== id));
+    } catch (error) {
+      console.error("Error deleting contact:", error);
+    }
   };
 
   return (
